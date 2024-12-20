f0945fa2c5bad2a492a5e12bacf2b1de34c57838
waleed-khalaf <waleed.k.khalaf@gmail.com>
2024-12-20 13:16:03 +0300

    limiting screen refresh to 60fps & adding circle object

--- main.py
+++ main.py
@@ -5,14 +5,25 @@ with contextlib.redirect_stdout(None):
 
 
 def main():
+	# Initialising game
 	pygame.init()
+	
+	# Create new clock object to maniuplate fps
+	clock = pygame.time.Clock()
+	# delta time to represent amount of time that has passed
+	dt = 0
+	# setting up screen
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
+		# allows user to quit by pressing "x" button on gui window
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		# fills up screen with black colour
 		screen.fill((0,0,0))
+		# updates the full display to the screen 
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
