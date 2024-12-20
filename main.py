132908c3ceca7d104eef45fff7676da67e4e4e25
waleed-khalaf <waleed.k.khalaf@gmail.com>
2024-12-20 12:26:24 +0300

    setting up screen for asteriods

---
+++ main.py
@@ -0,0 +1,18 @@
+from constants import *
+import contextlib
+with contextlib.redirect_stdout(None):
+	import pygame
+
+
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		screen.fill((0,0,0))
+		pygame.display.flip()
+
+if __name__ == "__main__":
+	main()
